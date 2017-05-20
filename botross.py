78d9651cd8f0b1bc1e5c41073e6398c2e9b0a0ec
Dibbo <DeflatedPickle@gmail.com>
2017-05-21 00:09:42 +0100

    Many updates

--- botross.py
+++ botross.py
@@ -1,5 +1,8 @@
-from discord.ext import commands
 import json
+from datetime import datetime
+
+
+from discord.ext import commands
 
 # https://discordapp.com/oauth2/authorize?client_id=313728091702951937&scope=bot&permissions=0
 
@@ -8,12 +11,26 @@ description = "A Discord bot that will make Bob Ross paint a given image."
 startup_extentions = ["cogs.botross-admin_commands", "cogs.botross-user_commands", "cogs.botross-paint", "cogs.botross-events"]
 
 bot = commands.Bot(command_prefix=commands.when_mentioned, description=description)
+bot.log_file = "/home/DeflatedPickle/BotRoss/log.txt"
+bot.credentials = "/home/DeflatedPickle/BotRoss/credentials.json"
 bot.remove_command("help")
 
 
+def write_to_log(text: str="", also_time=True, also_print=True):
+    if also_time:
+        time = datetime.now().strftime("%H:%M:%S")
+    with open(bot.log_file, "a") as log:
+        log.write("{} {}\n".format(str(time) + " |" if also_time else None + " |", text))
+        log.close()
+        if also_print:
+            print("{} {}".format(str(time) + " |" if also_time else None + " |", text))
+
+bot.write_to_log = write_to_log
+
+
 def load_credentials():
-    with(open(r"credentials.json")) as f:
-        return json.load(f)
+    with(open(bot.credentials)) as creds:
+        return json.load(creds)
 
 ##################################################
 
--- cogs/botross-paint.py
+++ cogs/botross-paint.py
@@ -11,17 +11,29 @@ class Paint:
 
     @commands.command(pass_context=True)
     async def paint_image(self, ctx, image: str="https://ih0.redbubble.net/image.353370368.9497/flat,800x800,075,f.u1.jpg", bobtype: int=0):
-        with open(r"downloaded_image.png", "wb") as download:
-            download.write(urllib.request.urlopen(image).read())
+        """Makes the bot paint a given image."""
+        with open("/home/DeflatedPickle/BotRoss/downloaded_image.png", "wb") as download:
+            try:
+                download.write(urllib.request.urlopen(image).read())
+            except urllib.error.HTTPError as error:
+                if error.code == 403:
+                    await self.bot.say("I could not access this happy little site.")
+                    return
+
+                elif error.code == 404:
+                    await self.bot.say("This happy little site does not exist.")
+                    return
+                else:
+                    print(error)
+                    await self.bot.say("I couldn't paint this happy little image.")
+                    return
             download.close()
-        image = thejoyofpynting.paint_a_picture(r"downloaded_image.png", bobtype)
-        image.save(r"image.png", "PNG")
 
-        await self.bot.send_file(ctx.message.channel, filename=r"image.png", content="{}, I've painted you a happy little image.".format(ctx.message.author.mention))
+        image_paint = thejoyofpynting.paint_a_picture("/home/DeflatedPickle/BotRoss/downloaded_image.png", bobtype)
+        image_paint.save("/home/DeflatedPickle/BotRoss/painted_image.png", "PNG")
 
-    @paint_image.error
-    async def paint_image_on_error(self, ctx):
-        await self.bot.send_message(ctx.message.channel, "I couldn't paint this happy little image.")
+        await self.bot.send_file(ctx.message.channel, fp="/home/DeflatedPickle/BotRoss/painted_image.png", content="{}, I've painted you this happy little image.".format(ctx.message.author.mention))
+        self.bot.write_to_log("Painted the file {} for {}.".format(image, ctx.message.author))
 
     @commands.command(pass_context=True)
     async def paint_me(self, ctx, bobtype: int=0):
